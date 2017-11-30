bf1c0423e2feb91f2ffe124c504be8a8ae84c6cf
wangxh <393760597@qq.com>
2017-11-30 18:08:24 +0800

    add qi and qv model todo

---
+++ model1/qi_model.py
@@ -0,0 +1,31 @@
+import math
+
+c15 = 2.249
+c16 = -0.001127
+c17 = 3.09
+c18 = -0.0002869
+alpha_1 = 2.249
+beta_1 = -0.001127
+alpha_2 = 3.09
+beta_2 = -0.0002869
+c19 = -0.00000000001689
+c20 = 0.0000002583
+c21 = -0.001508
+c22 = 5.211
+alpha_3 = 0.5852
+beta_3 = -3.641
+alpha_4 = 0.4139
+beta_4 = -0.009638
+
+
+# def quality_of_live_session(t_zapping, t, T=10):
+#     q_zapping = c15 * math.exp(c16 * t_zapping) + c17 * math.exp(c18 * t_zapping)
+#     qi_liveinstant = q_zapping
+#     qi_live_session = (alpha_1 * math.exp(beta_1 * t / T) + alpha_2 * math.exp(beta_2 * t / T)) * qi_liveinstant
+#     return qi_live_session
+#
+#
+# def quality_of_vod_session(t_loading, t, T):
+#     q_loading = c19 * (t_loading ** 3) + c20 * (t_loading ** 2) + c21 * t_loading + c22
+#     qi_vod_instant = q_loading
+#     qi_vod_session
